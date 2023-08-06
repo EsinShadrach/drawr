ae246ce8e4123eb76c1e0ace286b9a030f290a3c
EsinShadrach <esinshadrachrafe@gmail.com>
2023-08-07 00:19:58 +0100

    open graph

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import logo from "@/public/images/logo.png";
 export default function Home() {
 	return (
-		<div className="container px-2 mx-auto">
-			<Image src={logo} alt="Drawr Logo" draggable={"false"} />
+		<div className="mx-auto">
+			<section className="container px-2 mx-auto">apple</section>
 		</div>
 	);
 }
