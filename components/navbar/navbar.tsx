bc2ec2e7ab97c760b33f24edfe023b7a771f83e6
EsinShadrach <esinshadrachrafe@gmail.com>
2023-08-06 22:43:59 +0100

    fonts

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+	return (
+		<header>
+			<nav className="container px-2 mx-auto">
+				<div></div>
+				<div></div>
+				<div></div>
+			</nav>
+		</header>
+	);
+}
