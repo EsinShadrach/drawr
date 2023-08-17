9a5aa2874336d60725e1dbbc1e289a6be7b48941
Esin Shadrach <esinshadrachrafe@gmail.com>
2023-08-17 15:22:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Sponsors } from "./Sponsors";
 import { HeroSection } from "./HeroSection";
-import { Features } from "./Features";
+import { Features } from "./features";
 import { Gaming } from "./Gaming";
 import { JoinClub } from "./JoinClub";
 import { Awards } from "./Awards";
