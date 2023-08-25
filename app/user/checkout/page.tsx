ddf19365b786ea887a75887bdc6a8d4b50407520
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-25 02:30:16 +0100

    --sleep sir

--- app/user/checkout/page.tsx
+++ app/user/checkout/page.tsx
@@ -11,26 +11,34 @@ import React from "react";
 export default function CheckOut() {
   return (
     <section>
-      <div>
-        <div className="bg-white w-full h-[48px] shadow-[0px_2px_5px_0px_rgba(0, 0, 0, 0.25]"></div>
+      <div className="bg-white w-full h-[48px] shadow-[0px_2px_5px_0px_rgba(0, 0, 0, 0.25]">
+        <div className="h-[430px] p-3 w-full bg-[#7A71FE]/50 bg-[url(https://res.cloudinary.com/dkoe20rzl/image/upload/v1692923852/Image_1_tax86k.png)] bg-cover bg-[50%_50%] bg-blend-normal bg-no-repeat ">
+          <div>
+            <p
+              className={`text-base mt-[34px] text-right mr-5 font-abold font-aeonik text-[#fff] leading-[13.5px] tracking-[0.213px]`}
+            >
+              close
+            </p>
+          </div>
 
-        <div>
-          <p
-            className={`text-base mt-[34px] text-right mr-5 font-extrabold font-aeonik text-[#8C8C8C] leading-[13.5px] tracking-[0.213px]`}
-          >
-            close
-          </p>
-        </div>
-
-        <div>
-          <H3
-            className={`text-[18.36px] mt-[34px] text-left mr-5 font-extrabold font-aeonik text-[#7A71FE] ml-3`}
-          >
-            Ticket Plans
-          </H3>
+          <div className="">
+            <H3
+              className={`text-[35.9px] mt-[170px] font-bold text-left font-aeonik text-[#7A71FE] ml-3`}
+            >
+              Ticket Plans
+            </H3>
+          </div>
         </div>
 
         <div className="bg-[#7A71FE]/20"></div>
+      </div>
+
+      <div>
+        <div>
+          <h2 className="font-aeonik font-bold leading-[17.09px] capitalize tracking-[0.269px] text-black ">
+            Spagfest Raffle Event
+          </h2>
+        </div>
       </div>
     </section>
   );
